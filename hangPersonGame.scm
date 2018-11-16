609eb87d4821471bbb5200c4b4ef851f9115b7ec
mes11392 <44754028+mes11392@users.noreply.github.com>
2018-11-16 15:51:08 -0500

    update

--- hangPersonGame.scm
+++ hangPersonGame.scm
@@ -1,4 +1,11 @@
 #lang scheme
-(begin
-  (display "Hello, World!")
-  )
+(define (chooseWord)
+  (let ((lst '(programming imperative functional logic declarative scripting prolog lexical syntactical branching)))
+       (list-ref lst (random (length lst)))))
+
+(define (play)
+  (let ((word chooseWord))
+       (display (word))
+       (string->list word)
+       (for ([i word])
+             (display "_"))))
