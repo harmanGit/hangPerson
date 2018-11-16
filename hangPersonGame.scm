83f04dffde1a1ab14e64cc9be965719826b4fad6
mes11392 <44754028+mes11392@users.noreply.github.com>
2018-11-16 16:17:16 -0500

    update

--- hangPersonGame.scm
+++ hangPersonGame.scm
@@ -1,11 +1,17 @@
 #lang scheme
 (define (chooseWord)
-  (let ((lst '(programming imperative functional logic declarative scripting prolog lexical syntactical branching)))
+  (let ((lst '("programming" "imperative" "functional" "logic" "declarative" "scripting" "prolog" "lexical" "syntactical" "branching")))
        (list-ref lst (random (length lst)))))
 
 (define (play)
-  (let ((word chooseWord))
-       (display (word))
+  (let ((word (chooseWord)))
        (string->list word)
        (for ([i word])
-             (display "_"))))
+             (display "_ "))
+       (display "\n"))
+  (guess))
+
+(define (guess word)
+  (display "Guess a letter:")
+  (let ((letter (read)))
+       (if 
