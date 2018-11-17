b450bc2cb48ce33aefa4796ce0d6753f1566d280
mes11392 <44754028+mes11392@users.noreply.github.com>
2018-11-16 20:14:30 -0500

    fixed tries. Checking if user has guessed correct word not working.

--- hangPersonGame.scm
+++ hangPersonGame.scm
@@ -1,30 +1,44 @@
 #lang scheme
+
+; Chooses a word from a list of words at random
 (define (chooseWord)
   (let ((lst '("programming" "imperative" "functional" "logic" "declarative" "scripting" "prolog" "lexical" "syntactical" "branching")))
        (list-ref lst (random (length lst)))))
 
+; Used to start the game of Hangman
 (define (play)
-  (let ((word (chooseWord)) (blanks '("")))
+  (let ((word (chooseWord)) (blanks '()))
        (string->list word)
        (for ([i word])
              (display "_ ")
-             (set! blanks (append blanks '("_ "))))
+             (set! blanks (append blanks '("_"))))
     (display word)
-    (display blanks)
-    (display (length blanks))
-    (guess word blanks 10)))
+    (guess word blanks 0)))
 
+; Allows the user to guess a letter until they guess incorrectly 10 times 
 (define (guess word guessedList tries)
   (display "\n Guess a letter:")
   (let ((letter (read-line))
-        (count 1))
+        (count 0)
+        (wrong 0))
        (for ([i word])
          (if (string=? letter (make-string 1 i))
              (set! guessedList (list-set guessedList count letter))
-             (display ""))
+             (set! wrong (add1 wrong)))
          (set! count (add1 count)))
   (display guessedList)
+  (if (equal? wrong (length (string->list word)))
+      (set! tries (add1 tries))
+      (display ""))
+  (display "\n Tries:")
   (display tries)
-  (if (equal? tries 3)
-      (display "Sorry, you lost!")
-      (guess word guessedList tries))))
+  (display (string->list word))
+  (display guessedList))
+
+  ; Check if user has guessed the whole word DOESN'T WORK 
+  (if (equal? (string->list word) guessedList)
+      (display "Congratulations! You won!")
+      ; Check if user has guessed incorrectly 10 times 
+      (if (equal? tries 3)
+          (display "Sorry, you lost!")
+          (guess word guessedList tries))))
